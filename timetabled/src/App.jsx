d3eff73c5eeb452b50c86e43ba2b7a6d700e5ddb
Jacob Lai <102393117+solaceinthenight@users.noreply.github.com>
2023-09-15 13:44:01 -0700

    Step 2 complete

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Calendar from "./components/Calendar";
 
 const App = () => {
   return (
     <div className="App">
       <h1>A Week in the Life of Tommy 🌱</h1>
       <h2>Explore the week of a Computer Science student during summer.</h2>
+      <Calendar />
     </div>
   );
 };
