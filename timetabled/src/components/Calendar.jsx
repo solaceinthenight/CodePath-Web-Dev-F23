9557398d8493f644aa1d5ece33e4ffb6c65f7935
Jacob Lai <102393117+solaceinthenight@users.noreply.github.com>
2023-09-15 14:17:10 -0700

    Step 5 complete

--- timetabled/src/components/Calendar.jsx
+++ timetabled/src/components/Calendar.jsx
@@ -20,7 +20,7 @@ const Calendar = () => {
         <tbody>
           <tr>
             <td className="time">8 am</td>
-            <Event />
+            <Event event="Fancy Dinner 🎩" color="green" />
             <td></td>
             <td></td>
             <td></td>
