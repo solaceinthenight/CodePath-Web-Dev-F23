c005e56f7ea57bec0aa34831c4c67ac635417b47
Jacob Lai <102393117+solaceinthenight@users.noreply.github.com>
2023-09-27 15:17:18 -0700

    SamosaSelector Checkpoint 2 Complete

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier);
+
   return (
     <div className="App">
       <div className="header">
         <h1>Samosa Selector</h1>
-        <h2>Count:</h2>
-        <img className="samosa" src="./assets/samosa.png" />
+        <h2>Count: {count}</h2>
+        <img src="/samoosa.png" className="samosa" onClick={updateCount} />
       </div>
     </div>
   );
