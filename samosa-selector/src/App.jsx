a551eab745466e05a87f6265fbe5d3a9634fdb0d
Jacob Lai <102393117+solaceinthenight@users.noreply.github.com>
2023-09-27 15:07:05 -0700

    SamosaSelector Checkpoint 1

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count:</h2>
+        <img className="samosa" src="./assets/samosa.png" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
