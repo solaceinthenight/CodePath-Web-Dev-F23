a5642012e1a8dba438f62c670f7a98cafe8a0f0a
Jacob Lai <102393117+solaceinthenight@users.noreply.github.com>
2023-09-30 10:13:01 -0700

    BaristaApp Checkpoint 5 Complete

--- barista-app/src/Components/BaristaForm.jsx
+++ barista-app/src/Components/BaristaForm.jsx
@@ -82,67 +82,75 @@ const BaristaForm = () => {
           🔄
         </button>
       </div>
-      <h3>Temperature</h3>
-      <div className="answer-space" id={correct_temp}>
-        {inputs["temperature"]}
+      <div className="mini-container">
+        <h3>Temperature</h3>
+        <div className="answer-space" id={correct_temp}>
+          {inputs["temperature"]}
+        </div>
+        <RecipeChoices
+          handleChange={(e) =>
+            setInputs((prevState) => ({
+              ...prevState,
+              [e.target.name]: e.target.value,
+            }))
+          }
+          label="temperature"
+          choices={ingredients["temperature"]}
+          currentVal={inputs["temperature"]}
+        />
       </div>
-      <RecipeChoices
-        handleChange={(e) =>
-          setInputs((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-          }))
-        }
-        label="temperature"
-        choices={ingredients["temperature"]}
-        checked={inputs["temperature"]}
-      />
-      <h3>Syrup</h3>
-      <div className="answer-space" id={correct_syrup}>
-        {inputs["syrup"]}
+      <div className="mini-container">
+        <h3>Syrup</h3>
+        <div className="answer-space" id={correct_syrup}>
+          {inputs["syrup"]}
+        </div>
+        <RecipeChoices
+          handleChange={(e) =>
+            setInputs((prevState) => ({
+              ...prevState,
+              [e.target.name]: e.target.value,
+            }))
+          }
+          label="syrup"
+          choices={ingredients["syrup"]}
+          checked={inputs["syrup"]}
+        />
       </div>
-      <RecipeChoices
-        handleChange={(e) =>
-          setInputs((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-          }))
-        }
-        label="syrup"
-        choices={ingredients["syrup"]}
-        checked={inputs["syrup"]}
-      />
-      <h3>Milk</h3>
-      <div className="answer-space" id={correct_milk}>
-        {inputs["milk"]}
+      <div className="mini-container">
+        <h3>Milk</h3>
+        <div className="answer-space" id={correct_milk}>
+          {inputs["milk"]}
+        </div>
+        <RecipeChoices
+          handleChange={(e) =>
+            setInputs((prevState) => ({
+              ...prevState,
+              [e.target.name]: e.target.value,
+            }))
+          }
+          label="milk"
+          choices={ingredients["milk"]}
+          checked={inputs["milk"]}
+        />
       </div>
-      <RecipeChoices
-        handleChange={(e) =>
-          setInputs((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-          }))
-        }
-        label="milk"
-        choices={ingredients["milk"]}
-        checked={inputs["milk"]}
-      />
-      <h3>Blended</h3>
-      <div className="answer-space" id={correct_blended}>
-        {inputs["blended"]}
+      <div className="mini-container">
+        <h3>Blended</h3>
+        <div className="answer-space" id={correct_blended}>
+          {inputs["blended"]}
+        </div>
+        <RecipeChoices
+          handleChange={(e) =>
+            setInputs((prevState) => ({
+              ...prevState,
+              [e.target.name]: e.target.value,
+            }))
+          }
+          label="blended"
+          choices={ingredients["blended"]}
+          checked={inputs["blended"]}
+        />
       </div>
-      <RecipeChoices
-        handleChange={(e) =>
-          setInputs((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-          }))
-        }
-        label="blended"
-        choices={ingredients["blended"]}
-        checked={inputs["blended"]}
-      />
-      <form></form>
+      <form className="container"></form>
       <button type="submit" className="button submit" onClick={onCheckAnswer}>
         Check Answer
       </button>
