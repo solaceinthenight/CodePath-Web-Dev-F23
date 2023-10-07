32477f07a843061a528f1af2a3422d751a87a82b
Jacob Lai <102393117+solaceinthenight@users.noreply.github.com>
2023-10-07 10:48:21 -0700

    Cap Checkpoint 1 Complete

--- cap/src/App.jsx
+++ cap/src/App.jsx
@@ -1,11 +1,38 @@
 import { useState } from "react";
-import viteLogo from "/vite.svg";
+import APIForm from "./components/APIForm";
 import "./App.css";
 
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
 function App() {
-  return <></>;
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+
+  const submitForm = () => {};
+
+  return (
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
+
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+      />
+      <br></br>
+    </div>
+  );
 }
 
 export default App;
